0447cb7edf14b5d8c9260730d682bd26fa95daeb
Joe Politz <joe@cs.brown.edu>
2023-01-31 06:56:20 -0800

    update tests

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -7,15 +7,15 @@ import java.io.IOException;
 public class TestDocSearch {
 	@Test 
 	public void testIndex() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
+    Handler h = new Handler("./written_2/");
     URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 10 total files to search.", h.handleRequest(rootPath));
+    assertEquals("There are 224 total files to search.", h.handleRequest(rootPath));
 	}
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=taxation");
-    String expect = "Found 3 paths:\n./technical/biomed/1471-2296-3-3.txt\n./technical/government/Gen_Account_Office/d01591sp.txt\n./technical/plos/journal.pbio.0020052.txt";
+    Handler h = new Handler("./written_2/");
+    URI rootPath = new URI("http://localhost/search?q=sushi");
+    String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
